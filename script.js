e60f63d863a7aae108fe73f5b24d6237144daaba
Michael Wong <mizik_jah@yahoo.com>
2022-12-23 11:18:44 -0500

    dom manipulation

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.message').textContent = '🎉Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 14;
+console.log(document.querySelector('.guess').value);
