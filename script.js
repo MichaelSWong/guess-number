1f7874852f2ef58c9e14dd318eedfd54f88527d7
Michael Wong <mizik_jah@yahoo.com>
2022-12-23 13:41:48 -0500

    add reset

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.querySelector('.check').addEventListener('click', getGuess);
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.again').addEventListener('click', reset);
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
@@ -7,11 +9,19 @@ function getGuess() {
   const guess = Number(document.querySelector('.guess').value);
   console.log({ secretNumber });
 
+  // When their is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // When Player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Guess!';
     document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '👇 Too High!';
@@ -22,6 +32,8 @@ function getGuess() {
       document.querySelector('.check').setAttribute('disabled', true);
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '👆 Too Low!';
@@ -33,4 +45,15 @@ function getGuess() {
       document.querySelector('.score').textContent = 0;
     }
   }
+}
+
+function reset() {
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing..';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 }
